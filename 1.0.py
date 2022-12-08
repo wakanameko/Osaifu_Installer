00358bcf600071fbb3dd4d318fd1b89cb13ae0cd
wakanameko <63937252+wakanameko@users.noreply.github.com>
2022-12-08 16:04:27 +0900

    後藤さんチンポでかいのねぇ〜
    わっかるー！！

--- 1.0.py
+++ 1.0.py
@@ -1,12 +1,26 @@
 vertion = "1.0"
 developer = "@wakanameko2"
 
+import os
+import platform
 import sys
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
+if not os.name == 'nt':messagebox.showerror('Attention','Windows以外のOSでの実行は想定されていません。エラーが発生しても自己責任でお願いします。')
+
+if ur.release == 'vista':messagebox.showerror('Attention','このバージョンのWindowsは対応していません。')
+if ur.release == 'xp':messagebox.showerror('Attention','このバージョンのWindowsは対応していません。')
+if ur.release == '2000':messagebox.showerror('Attention','このバージョンのWindowsは対応していません。')
+if ur.release == 'me':messagebox.showerror('Attention','このバージョンのWindowsは対応していません。')
+if ur.release == '98':messagebox.showerror('Attention','このバージョンのWindowsは対応していません。')
+if ur.release == '95':messagebox.showerror('Attention','このバージョンのWindowsは対応していません。')
+
 def exit_tab():
   print('exit')
+
+label = QLabel()
+label.setText('このソフトフェアを使用するにはADBが必要です。')
 
 app = QApplication(sys.argv)
 main = QMainWindow()
